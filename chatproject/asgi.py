34113462571b8c8e278f7c0a4798d7619a95a875
ShahriyarHussain <shossainshihab36@gmail.com>
2021-08-07 17:34:46 +0600

    Add room view and setup consumer

--- chatproject/asgi.py
+++ chatproject/asgi.py
@@ -6,16 +6,24 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
-
 import os
 import django
+
+
+from channels.auth import AuthMiddlewareStack
 from channels.http import AsgiHandler
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
   "http": AsgiHandler(),
-  # Just HTTP for now.
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+  ),
 })
--- chatproject/settings.py
+++ chatproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5%u5kfsv6p@3ik@zxp7vnvmm=nlpqoxsnwviutnqlcdsdvzrqu'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY_CHATAPP')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chatproject.wsgi.application'
+#channels
 ASGI_APPLICATION = 'chatproject.asgi.application'
 
 
