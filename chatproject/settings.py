a1747a572cbcd755f9aea182afeb4c18c68e28d6
ShahriyarHussain <shossainshihab36@gmail.com>
2021-08-08 08:52:47 +0600

    Test chat functionality using selenium

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -89,9 +89,13 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
+
 
 
 # Password validation
